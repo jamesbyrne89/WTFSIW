e25e2baf36fc1b89e51f32331fbbb5911ced62c0
jamesbyrne89 <mail@jamestbyrne.com>
2017-01-16 23:10:59 +0000

    Added responsive styles

--- scripts/movies.js
+++ scripts/movies.js
@@ -3,6 +3,13 @@ const endpoint = 'https://random-movie.herokuapp.com/random';
 const movies = [];
 
 function getJSON(){
+		document.querySelector('.left-line').classList.add('off-left');
+		
+		setTimeout(function() {
+		document.querySelector('.left-line').classList.add('hide');
+		document.querySelector('.subtitle').classList.add('sub-small');
+		document.querySelector('.main-title').classList.add('shrunk');
+	}, 500);
 		document.querySelector('.movie-holder').classList.remove('visible');
 	document.querySelector('.movie-holder').classList.add('invisible');
 		document.querySelector('.movie-holder').classList.add('off-right');
@@ -14,38 +21,44 @@ function getJSON(){
 		document.querySelector('.suggestion').classList.remove('visible');
 		document.querySelector('.suggestion').classList.remove('hide');
 		document.querySelector('.try-again').classList.remove('hide');
-		document.querySelector('.left-line').classList.add('off-left');
-	setTimeout(function() {
 
-	$.getJSON('https://random-movie.herokuapp.com/random', (data) => {
+
+	setTimeout(function() {
+$.ajax({
+    url: 'https://random-movie.herokuapp.com/random',
+    crossDomain: true,
+    dataType: 'jsonp',
+    success: function(data) {
 		let source = document.createAttribute("src");
 		source.value=(data.Poster);
+		console.log(data);
 		document.querySelector('.poster').setAttributeNode(source);
-		document.querySelector('.suggestion').innerHTML=(`You should watch <span class="suggestion-name">${data.Title}</span>`);
-		document.querySelector('.runtime').textContent=(data.Runtime);
-		document.querySelector('.year').textContent=(data.Year);
-		document.querySelectorAll('.divider').textContent=('/');
+		document.querySelector('.suggestion').innerHTML=(`Why don't you watch <span class="suggestion-name">${data.Title}</span>`);
+		document.querySelector('.top-info-wrapper').innerHTML=(
+			`<span class="year">${data.Year}</span>
+			<span class="divider">/</span>
+			<span class="genre">${data.Genre}</span>
+			<span class="divider">/</span>
+			<span class="runtime">${data.Runtime}</span>`);
 		document.querySelector('.genre').textContent=(data.Genre);
 		document.querySelector('.plot').textContent=(data.Plot);
 		document.querySelector('.rating').innerHTML=(`IMDB rating: <span class="num">${data.imdbRating}</span>`);
 		document.querySelector('.actors').textContent=(data.Actors);
-		document.querySelector('.main-title').classList.add('shrunk');
-document.querySelector('.left-line').classList.add('hide');
-		
-		document.querySelector('.subtitle').classList.add('sub-small');
 		document.querySelector('.suggestion').classList.add('visible');
 	document.querySelector('.suggestion').classList.remove('invisible');	
 		
 		document.querySelector('.movie-holder').classList.remove('off-right');
 	document.querySelector('.movie-holder').classList.add('visible');
 	document.querySelector('.movie-holder').classList.remove('invisible');
-})
+	   }
+  });
+
 		}, 1000);
 
 	setTimeout(function() {
 document.querySelector('.try-again').classList.remove('invisible');
 		document.querySelector('.try-again').classList.add('visible');	
-	}, 3000);
+	}, 2000);
 
 }
 
@@ -57,4 +70,4 @@ document.onkeydown = function(e){
 };
 
 
-document.querySelector('.try-again').addEventListener('click', getJSON);
+document.querySelector('.try-again a').addEventListener('click', getJSON);
